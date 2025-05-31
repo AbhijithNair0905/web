f9e637af14c161efad475eeb6bf351d0ddcd0ed4
Yurvaj Darekar <Yurvaj Darekar@nangiavp.local>
2025-05-31 18:32:29 +0530

    fix: about

--- src/components/sections/summery.jsx
+++ src/components/sections/summery.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import profile from "../../assets/images/about/profile.png"
 import { RiMailSendLine } from '@remixicon/react'
 import SlideUp from '../../utlits/animations/slideUp'
+import { Link } from 'react-router-dom'
 const Summery = () => {
     return (
         <section id="about" className="about-single-area innerpage-single-area">
@@ -21,12 +22,12 @@ const Summery = () => {
                         <SlideUp>
                             <div className="about-content-part">
                                 <h2>
-                                    I’m Bentos Walker, a product designer.
+                                    I’m Abhijith Nair, a designer.
                                 </h2>
-                                <p>I am a San francisco-based product designer with a focus on web design, illustration, a visual development. I have a diverse range of experience having worked across various fields and industries.</p>
-                                <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source.</p>
+                                <p>I am a Mumbai-based designer with a focus on product design, illustration, a visual development. I have a diverse range of experience having worked across various fields and industries.</p>
+                                <p>My work is driven by a passion for creating meaningful and impactful experiences. I believe in the power of design to solve problems and enhance the way we interact with the world around us.</p>
                                 <div className="hero-btns">
-                                    <a href="contact.html" className="theme-btn">Get In touch<i> <RiMailSendLine size={16} /> </i></a>
+                                    <Link to="/contact" className="theme-btn">Get In touch<i> <RiMailSendLine size={16} /> </i></Link>
                                 </div>
                             </div>
                         </SlideUp>
